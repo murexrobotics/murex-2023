edc84369506f9c958c09d4a681d2b87055f70e71
Celine <celine.zhang2007@gmail.com>
2023-03-17 09:55:26 -0700

    FIX(CAMERA): improved latency and video quality; optimized ffplay

--- Camera/rpi.sh
+++ Camera/rpi.sh
@@ -1,1 +1,1 @@
-ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -b:v 125000 -fflags nobuffer -flags low_delay -preset ultrafast -tune zerolatency -probesize 32 -num_output_buffers 32 -num_capture_buffers 16 -analyzeduration 0 -f mpegts udp://192.168.100.52:1234
+ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -vf scale=1440x900 -b:v 3000k -fflags nobuffer -flags low_delay -preset ultrafast -tune zerolatency -probesize 32 -num_output_buffers 32 -num_capture_buffers 16 -analyzeduration 0 -f mpegts udp://192.168.100.51:1234
