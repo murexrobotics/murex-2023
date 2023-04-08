ea475b89898db94a7d9c27b8d63420de8f50c45e
L0ad1n6 <altan.mehmet.unver@gmail.com>
2023-04-08 17:34:41 -0400

    Refactor(Testing): Better logging + testing moved function

--- Integration/aquatic/integration/pca9685.py
+++ Integration/aquatic/integration/pca9685.py
@@ -19,16 +19,28 @@ Constants:
     CAMERA_SERVO_CHANNELS (PCAChannels): PWM Channels to control camera angle
 """
 
+import logging
 import atexit
 from adafruit_pca9685 import PCA9685
-from .i2c import i2c
+from i2c import i2c
 
+logging.info("Initializing PCA9685")
 pca = PCA9685(i2c)
 pca.frequency = 50
 
+logging.debug("Allocating PWM Channels")
 THRUSTER_CHANNELS = pca.channels[:6]
 ARM_BLDC_CHANNELS = pca.channels[6:10]
 CAMERA_SERVO_CHANNEL = pca.channels[10]
+
+def _test():
+    """Tests PCA9685 initialization"""
+    logging.info("Testing PCA9685 initialization")
+    assert pca is not None, "PCA9685 not initialized"
+    assert i2c.scan() != [], "No I2C devices found, PCA Not registered"
+    assert len(THRUSTER_CHANNELS) == 6, "Incorrect number of thruster channels"
+    assert len(ARM_BLDC_CHANNELS) == 4, "Incorrect number of arm BLDC channels"
+    logging.info("PCA9685 initialized successfully")
 
 def _stop():
     """Deinitializes PCA9685"""
@@ -36,18 +48,12 @@ def _stop():
 
 # Make sure that PCA9685 is gracefully shut off so that other 
 # components can also be gracefully stopped.
+logging.debug("Registering PCA9685 deinitialization")
 atexit.register(_stop)
 
 if __name__ == '__main__':
-    """Tests PCA9685 initialization"""
-    import logging
     logging.basicConfig(level=logging.DEBUG)
-    logging.info("Testing PCA9685 initialization")
-
-    assert pca is not None, "PCA9685 not initialized"
-    assert i2c.scan() != [], "No I2C devices found, PCA Not registered"
-
-    logging.info("PCA9685 initialized successfully")
+    _test()
     
 
 
