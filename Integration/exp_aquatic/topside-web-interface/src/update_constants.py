c5a9227c7c908d6f4506b5410f7af69faa856317
dodoleonarnci <davidozhang0316@gmail.com>
2023-04-16 14:02:48 -0400

    Fix(Topside): Backup Signals sent through CGI script

--- Integration/exp_aquatic/topside-web-interface/src/update_constants.py
+++ Integration/exp_aquatic/topside-web-interface/src/update_constants.py
@@ -1,13 +1,9 @@
-import cgi
-import cgitb
+from flask import Flask,request,render_template,redirect
 import socket
 import sys
 
 ip = "192.168.100.1"
 port = 5678
-
-#cgitb.enable()
-form = cgi.FieldStorage()
 
 command_list = {
     'Restart_Pi?': 0,
@@ -15,15 +11,22 @@ command_list = {
     'Restart_Camera?': 0
 }
 
-for status_name in command_list:
-    change = form.getvalue(status_name)
+app = Flask(__name__)
+
+@app.route('/report',methods = ["POST"])
+def upload_image():
+    change = request.files[status_name]
     if change == "Yes" or "Y" or "y":
         command_list[status_name] = 1
     elif change.isnumeric() == True:
         command_list[status_name] = str(change)
     elif change == "No" or "N" or "n":
         command_list[status_name] = "INVALID INPUT"
+    return render_template("/routes/+page.svelte",filename=filename)
 
+@app.route('/display/<filename>')
+
+app.run(debug=True,port=2000)
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
     print("connection established to rpi")
