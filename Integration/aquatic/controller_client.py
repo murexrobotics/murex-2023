a3e47e07cc6d0f1349c49cd5a75fe4675eac5db3
L0ad1n6 <altan.mehmet.unver@gmail.com>
2023-04-17 16:04:36 -0400

    Update: Thruster update loop

--- Integration/aquatic/controller_client.py
+++ Integration/aquatic/controller_client.py
@@ -2,17 +2,22 @@ import asyncio
 import websockets
 import json
 
-async def listen(thrusters):
-    uri = "ws://localhost:6789" # TODO: CHANGE TO COMPUTER IP
+IP = "192.168.100.54"
+PORT = 6789
 
-    if not hasattr(thrusters, "_thruster_targets"):
+async def listen(thrusters):
+    uri = f"ws://{IP}:{PORT}"
+
+    if not hasattr(thrusters, "_thrust_targets"):
         raise Exception("Thrusters not initialized")
     
     async with websockets.connect(uri) as websocket:
         while True:
             message = await websocket.recv()
             data = json.loads(message)
-            thrusters._thruster_targets = [data["fr"], data["fl"], data["bl"], data["br"], data["v"], data["v"]]
+
+            # TODO: Add proppper interpolation
+            thrusters.set_thrusts(data["fr"], data["fl"], data["bl"], data["br"], data["v"], data["v"])
 
 if __name__ == "__main__":
     asyncio.run(listen())
--- Integration/aquatic/thrusters.py
+++ Integration/aquatic/thrusters.py
@@ -1,9 +1,3 @@
-# v1: 12 front
-# v2: 15
-# fr: 4
-# fl: 14
-# br: 5
-# bl: 3
 """
 Contains all code nececary to control thrusters. Use the methods 
 to interact with thrusters, rather than interacting directly 
@@ -48,12 +42,17 @@ Functions:
     set_thrusts(*thrusts):
         Sets the thrust of each thruster
 
-    adjust_magnitudes(gamepad: Gamepad):
-        Adjusts the thrust of each thruster based on the gamepad
+    simple_interpolate(target, duration):
+        Interpolates all thrusters to the target thrust over the duration
+    
+    complex_interpolate(targets, durations):
+        Every time it is called it gradually brings thrusters closer to actual thrust target
+
+    start_listening():
+        Starts a thread that listens for gamepad input and updates thrusters accordingly
 
     telemetry():
         Returns a dictionary of telemetry data
-
 """
 
 from logger import logger
@@ -62,7 +61,6 @@ import atexit
 import math
 import threading
 
-from gamepad import Gamepad
 from pca9685 import THRUSTER_CHANNELS
 from adafruit_pca9685 import PWMChannel
 
@@ -74,8 +72,6 @@ MAX_PLUSE_WIDTH = 1900
 STOP_DUTY_CYCLE = 5232
 MAX_DUTY_CYCLE = 6880 # IMPORTANT: Subject to change, this was the max achieved in testing but powersupply could not supply sufficient current current
 MIN_DUTY_CYCLE = 3600 # IMPORTANT: Subject to change, this was the max achieved in testing but powersupply could not supply sufficient current current
-
-# Actually initialize thrusters
 
 SPEED_UP_TIME = 2 # Time it takes to go from 0 to 1 in seconds
 
@@ -115,7 +111,6 @@ thruster_thread = None
 time.sleep(THRUSTER_INIT_TIME)
 logger.info("Thrusters Initialized")
 
-# Thruster Methods
 def set_thrusts(*thrusts: float):
     """Sets the thrust of each thruster, raises error if invalid number of thrusts is passed"""
     if len(thrusts) != len(_thrusters):
@@ -129,7 +124,6 @@ def set_thrusts(*thrusts: float):
 
 def simple_interpolate(target, duration):
     """Interpolates between min_value and max_value in steps steps over time time"""
-
     steps = duration * 10
     current = _thrusters[0].throttle
     step_size = (target - current) / steps
@@ -141,41 +135,47 @@ def simple_interpolate(target, duration):
         time.sleep(duration / steps)
 
 def complex_interpolation_step(*targets):
+    """Brings each thruster closer to its target"""
     for i, target in enumerate(targets):
         current_thrust = _thrusters[i].throttle
         _thrusters[i].throttle = math.copysign(1, target - current_thrust) * (0.05 / SPEED_UP_TIME)
 
 def telemetry():
-    return { "thruster": {
-        "fr": _thrusters[0].throttle,
-        "fl": _thrusters[1].throttle,
-        "br": _thrusters[2].throttle,
-        "bl": _thrusters[3].throttle,
-        "v1": _thrusters[4].throttle,
-        "v2": _thrusters[5].throttle
-    }}
-
-def _thruster_event_loop():
-    # TODO: Expand further
-    while True:
-        complex_interpolation_step(*_thrust_targets)
-        time.sleep(0.05)
+    return { 
+        "thruster": {
+            "fr": _thrusters[0].throttle,
+            "fl": _thrusters[1].throttle,
+            "br": _thrusters[2].throttle,
+            "bl": _thrusters[3].throttle,
+            "v1": _thrusters[4].throttle,
+            "v2": _thrusters[5].throttle
+        }
+    }
 
 def start_listening():
     global thruster_thread
+
+    def _thruster_event_loop():
+        # TODO: Expand further
+        while True:
+            complex_interpolation_step(*_thrust_targets)
+            time.sleep(0.05)
+
     thruster_thread = threading.Thread(target=_thruster_event_loop)
 
 def _stop():
     """Stops all thrusters"""
     logger.info("Stopping thrusters")
     set_thrusts(0, 0, 0, 0, 0, 0)
+    time.sleep(0.3) # Give time for all thrusters to stop
 
     try:
         thruster_thread.join()
     except:
+        logger.warn("Thruster update thread was not started")
         pass # This is a bad pattern but it just means that program exited before thruster thread was started
 
-    time.sleep(1) # Give time for all thrusters to stop
+    
 
 # Gracefully stop thrusters on exit
 atexit.register(_stop)
